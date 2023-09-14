8b9af8cd96ddd4e3d2f8ed2de967085495dd0a96
Avindu Dharmawardhana <137596644+GitAvi001@users.noreply.github.com>
2023-09-14 22:40:24 +0530

    added backend and frontend files

---
+++ back_end/connection.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const connectionStr = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PW}@cluster0.5xsbbpv.mongodb.net/newuser1?retryWrites=true&w=majority`;
+
+mongoose.connect(connectionStr, {useNewUrlparser: true})
+.then(() => console.log('connected to mongodb'))
+.catch(err => console.log(err))
+
+mongoose.connection.on('error', err => {
+  console.log(err)
+})
+
+// newuser1 - ZOoB1wTo85XOwUXW
